03583ce121822d3e642172b4a6de2dac18d9b82a
Ben Littleton <benlittleton@carfax.com>
2021-10-01 16:25:07 -0400

    Not sure what I actually changed in here :)

--- index.ts
+++ index.ts
@@ -4,11 +4,11 @@ import { promisify } from "util";
 import { scrapeAllSpellsFromCSV } from "./scrapers/spells";
 import { scrapeTraits } from "./scrapers/traits";
 
-// scrapeAllSpellsFromCSV()
-//   .then((data) => JSON.stringify(data))
-//   .then((jsonData) =>
-//     promisify(writeFile)(resolve(__dirname, "output", "spells.json"), jsonData)
-//   );
+scrapeAllSpellsFromCSV()
+  .then((data) => JSON.stringify(data))
+  .then((jsonData) =>
+    promisify(writeFile)(resolve(__dirname, "output", "spells.json"), jsonData)
+  );
 
 scrapeTraits()
   .then((data) => JSON.stringify(data))
